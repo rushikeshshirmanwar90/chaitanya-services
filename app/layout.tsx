e02ff7371f4091013c16efb0710c333d2c4ebee5
Mahesh Giri <girimahesh1514@gmail.com>
2025-08-14 20:01:18 +0530

    adding some margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className="ml-2 mr-2" >
+          {children}
+        </div>
       </body>
     </html>
   );
